5cd62273eeb97b77681eafe56625255f30b3dc67
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2022-12-09 11:45:02 +1100

    tweaks

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,7 +9,8 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gpush='git push origin HEAD'
+alias gpull='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
@@ -17,6 +18,7 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gfco='git fetch origin $1 && git checkout $1'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
