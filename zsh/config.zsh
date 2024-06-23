31f6c19a4c837d3b7f8d9e5bb207402f26e4df48
Abdelrahman Ahmed <181392+abdel@users.noreply.github.com>
2024-06-23 19:56:12 +0100

    better history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,6 +8,7 @@ autoload -U $DOTFILES/functions/*(:t)
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+HIST_STAMPS="yyyy-mm-dd"
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
@@ -15,7 +16,7 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+setopt SHARE_HISTORY # share history between sessions
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
